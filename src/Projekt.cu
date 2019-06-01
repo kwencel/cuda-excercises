d98e0a5ca61e4194ac7774661f6d26eb98cf393b
Krzysztof Wencel <kwencel@users.noreply.github.com>
2019-06-01 23:25:43 +0200

    WIP project assignment. #4

--- src/Projekt.cu
+++ src/Projekt.cu
@@ -12,16 +12,10 @@
 #include <thrust/device_vector.h>
 #include <thrust/copy.h>
 #include <thrust/count.h>
+#include <sstream>
 
 #include "device_launch_parameters.h"
 #include "CudaUtils.h"
-
-#define WORK_WIDTH 6 // TODO liczba wariacji
-#define WORK_HEIGHT 1
-#define BLOCK_WIDTH 1
-#define BLOCK_HEIGHT 1
-#define WORK_TOTAL WORK_WIDTH * WORK_HEIGHT
-#define RESULTS_TOTAL 10
 using Type = int;
 
 template <typename Container>
@@ -42,33 +36,48 @@ std::string printContainer(Container const& container) {
 
 
 template <typename Integral, typename std::enable_if_t<std::is_integral<Integral>::value>* = nullptr>
-__host__ __device__ Integral factorial(Integral const n) {
-//    if constexpr (!std::is_unsigned<Integral>::value) {
-    assert(n >= 0);
-//    }
+__host__ __device__ std::size_t factorial(Integral const n) {
+    #if __cplusplus >= 201703L
+    if constexpr (!std::is_unsigned<Integral>::value) {
+    #endif
+        assert(n >= 0);
+    #if __cplusplus >= 201703L
+    }
+    #endif
     if (n == 0) {
         return 1;
     }
-    Integral result = 1;
+    std::size_t result = 1;
     for (Integral i = 2; i <= n; ++i) {
         result *= i;
     }
+    assert(result > 0 && "Overflow detected!");
     return result;
 }
 
 template <typename Integral, typename std::enable_if_t<std::is_integral<Integral>::value>* = nullptr>
-__host__ __device__ Integral variationsCount(Integral const n, Integral const k) {
-//    if constexpr (!std::is_unsigned<Integral>::value) {
+__host__ __device__ std::size_t variationsCount(Integral const n, Integral const k) {
+    #if __cplusplus >= 201703L
+    if constexpr (!std::is_unsigned<Integral>::value) {
+    #endif
         assert(n >= 0 && k >= 0);
-//    }
+    #if __cplusplus >= 201703L
+    }
+    #endif
+    assert(n >= k);
     return factorial(n) / factorial(n - k);
 }
 
 template <typename T, typename Integral, typename std::enable_if_t<std::is_integral<Integral>::value>* = nullptr>
 __host__ __device__ void computeVariation(T const* const input, Integral const n, Integral const k, Integral p, T* const output) {
-    assert(n >= 0 && k >= 0 && k <= n && p >= 0 && p < variationsCount(n, k));
+    #if __cplusplus >= 201703L
+    if constexpr (!std::is_unsigned<Integral>::value) {
+    #endif
+        assert(n >= 0 && k >= 0 && k <= n && p >= 0 && p < variationsCount(n, k));
+    #if __cplusplus >= 201703L
+    }
+    #endif
 
-    // TODO possible optimization - use bitset
     bool* removed = new bool[k];
     for (Integral i = 0; i < k; ++i) {
         removed[i] = false;
@@ -126,13 +135,13 @@ __host__ __device__ bool checkPattern(GpuData<T, S> const& sequence, GpuData<T, 
         if (*patternPtr == *sequencePtr) {
             ++patternPtr;
             if (patternPtr - pattern.data == pattern.length) {
-                printf("[GTID %d] Matches!\n", gtid);
+                printf("[GTID %d] Matches! %d %d %d %d %d %d %d %d %d %d %d\n", gtid, patternPtr[0], patternPtr[1], patternPtr[2], patternPtr[3], patternPtr[4], patternPtr[5], patternPtr[6], patternPtr[7], patternPtr[8], patternPtr[9], patternPtr[10]);
                 return true;
             }
         }
         ++sequencePtr;
     }
-    printf("[GTID %d] Not matches!\n", gtid);
+//    printf("[GTID %d] Not matches!\n", gtid);
     return false;
 }
 
@@ -175,13 +184,26 @@ struct FoundMatcher : public thrust::unary_function<S, bool> {
 
 using namespace thrust::placeholders;
 
-int main() {
+template <class DestContainer, class Source>
+DestContainer parseTo(Source const& source) {
+    using Target = typename DestContainer::value_type;
+    std::istringstream is(source);
+    return DestContainer(std::istream_iterator<Target>(is), std::istream_iterator<Target>());
+}
 
-    std::vector<Type> pattern = { 0,1,1,0 };
+int main(int argc, char** argv) {
+    if (argc < 4) {
+        std::cerr << "Too few arguments. Usage: " << argv[0] << " <[any/all]> <pattern> <sequence>."
+                     "Pattern and sequence should be quoted lists of integers seperated by whitespaces.";
+        exit(1);
+    }
+
+    std::string mode = argv[1];
+    auto pattern = parseTo<std::vector<Type>>(argv[2]);
     CudaBuffer<Type, Type> devPattern(pattern);
-
-    std::vector<Type> sequence = { 1,2,4,3,5,3,6,2,1 };
+    auto sequence = parseTo<std::vector<Type>>(argv[3]);
     CudaBuffer<Type, Type> devSequence(sequence);
+    assert(pattern.size() <= sequence.size());
 
     std::vector<Type> distinctPattern(pattern);
     distinctValues(distinctPattern);
@@ -192,31 +214,70 @@ int main() {
     CudaBuffer<Type, Type> devDistinctSequence(distinctSequence);
 
     Type workAmount = variationsCount(distinctSequence.size(), distinctPattern.size());
-    thrust::device_vector<Type> devOutputVariations(workAmount * sizeof(Type));
+    std::cout << "[INFO] Variations to check: " << workAmount << std::endl;
+
+    thrust::device_vector<Type> devOutputVariations(workAmount * pattern.size());
     thrust::device_vector<bool> devOutputFound(workAmount);
 
-    dim3 dimBlock(workAmount, 1);
-    dim3 dimGrid(
-            static_cast<int>(ceilf(workAmount / static_cast<float>(dimBlock.x))),
-            static_cast<int>(ceilf(1 / static_cast<float>(dimBlock.y)))
-    );
+    /** ---------------------------- Calculate optimal kernel launch parameters ---------------------------- **/
+    int blockSize;   // The launch configurator returned block size
+    int minGridSize; // The minimum grid size needed to achieve the maximum occupancy for a full device launch
+    int gridSize;    // The actual grid size needed, based on input size
+    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, compute<Type, Type>, 0, workAmount);
 
-    printf("Invoking with: Block(%d,%d), Grid(%d,%d)\n", dimBlock.x, dimBlock.y, dimGrid.x, dimGrid.y);
-    runWithProfiler([&]{
-        compute<Type, Type><<<dimGrid, dimBlock>>>(
-                devSequence, devDistinctSequence, devPattern, devDistinctPattern,
-                devOutputVariations.data().get(), devOutputFound.data().get(), workAmount);
-    });
+    gridSize = (workAmount + blockSize - 1) / blockSize;
+    printf("[CUDA] Optimal block size: %d, grid size: %d\n", blockSize, minGridSize);
+    dim3 dimBlock(blockSize, 1);
+    dim3 dimGrid(gridSize, 1);
+    printf("[CUDA] Invoking with: Block(%d,%d), Grid(%d,%d)\n", dimBlock.x, dimBlock.y, dimGrid.x, dimGrid.y);
 
-    auto variationsAmount = thrust::count(devOutputFound.begin(), devOutputFound.end(), true);
-    thrust::device_vector<Type> devResult(variationsAmount * pattern.size());
-    auto trans = thrust::make_transform_iterator(thrust::counting_iterator<Type>(0), FoundMatcher<Type>(pattern.size(), devOutputFound.data().get()));
-    thrust::copy_if(devOutputVariations.begin(), devOutputVariations.end(), trans, devResult.begin(), _1 == true);
-//    thrust::copy_if(devOutputVariations.begin(), devOutputVariations.end(), thrust::make_permutation_iterator(devOutputFound.begin(), thrust::make_transform_iterator(thrust::counting_iterator<int>(0), _1 / pattern.size() == 1)), devResult.begin(), _1 == 1);
+    // Calculate theoretical occupancy
+    int maxActiveBlocks;
+    cudaOccupancyMaxActiveBlocksPerMultiprocessor(&maxActiveBlocks, compute<Type, Type>, blockSize, 0);
+    std::cout << maxActiveBlocks << std::endl;
+    int device;
+    cudaDeviceProp props;
+    cudaGetDevice(&device);
+    cudaGetDeviceProperties(&props, device);
+    float occupancy = (maxActiveBlocks * blockSize / props.warpSize) /(float)(props.maxThreadsPerMultiProcessor / props.warpSize);
+    printf("[CUDA] Theoretical occupancy of one SM: %f\n", occupancy);
+    printf("[CUDA] Throretical occupancy of whole GPU: %f\n", occupancy * ((float) gridSize / minGridSize));
 
-    thrust::host_vector<Type> result(devResult);
+    if (mode == "all") {
+        /** ---------------------------------------- Launch the kernel ---------------------------------------- **/
+        runWithProfiler([&] {
+            compute<Type, Type> <<<gridSize, blockSize>>> (
+                    devSequence, devDistinctSequence, devPattern, devDistinctPattern,
+                    devOutputVariations.data().get(), devOutputFound.data().get(), workAmount
+            );
+        });
 
-    std::cout << "PRZED " << printContainer(devOutputVariations) << std::endl;
-    std::cout << "PO " << printContainer(devResult) << std::endl;
+        /** ----------------------------------------- Process results ----------------------------------------- **/
+        auto variationsCorrect = thrust::count(devOutputFound.begin(), devOutputFound.end(), true);
+        thrust::device_vector<Type> devResult(variationsCorrect * pattern.size());
+        auto predicateSource = thrust::make_transform_iterator(thrust::counting_iterator<Type>(0),
+                                                               FoundMatcher<Type>(pattern.size(), devOutputFound.data().get()));
+        thrust::copy_if(devOutputVariations.begin(), devOutputVariations.end(), predicateSource, devResult.begin(), _1 == true);
+
+//        thrust::copy_if(
+//                devOutputVariations.begin(),
+//                devOutputVariations.end(),
+//                thrust::make_permutation_iterator(devOutputFound.begin(),
+//                                                  thrust::make_transform_iterator(thrust::counting_iterator<int>(0),
+//                                                                                  _1 / pattern.size() == 1)),
+//                devResult.begin(), _1 == 1
+//        );
+
+        thrust::host_vector<Type> result(devResult);
+        std::cout << "Found " << variationsCorrect << " patterns" << std::endl;
+        for (std::size_t resultIdx = 0; resultIdx < result.size(); resultIdx += pattern.size()) {
+            for (std::size_t i = 0; i < pattern.size(); ++i) {
+                std::cout << result[resultIdx + i] << " ";
+            }
+            std::cout << std::endl;
+        }
+    } else if (mode == "any") {
+
+    }
 
 }
